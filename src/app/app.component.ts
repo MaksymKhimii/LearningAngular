263fdb0b02a249226bf58a20f383f4d5303e780b
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-02 17:52:19 +0200

    Learning the basics and syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+    arr = [1, 1, 2, 3, 5, 8, 13];
+    objs = [
+        {
+            title: 'Post 1', author: 'Vladilen', comments: [
+                {name: 'Max1', text: 'Loren1'},
+                {name: 'Max1', text: 'Loren2'},
+                {name: 'Max1', text: 'Loren3'},
+            ]
+        },
+        {
+            title: 'Post 2', author: 'Vladilen 2', comments: [
+                {name: 'Max2', text: 'Loren1'},
+                {name: 'Max2', text: 'Loren2'},
+                {name: 'Max2', text: 'Loren3'},
+            ]
+        }
+    ];
+
+    now: Date = new Date();
 }
