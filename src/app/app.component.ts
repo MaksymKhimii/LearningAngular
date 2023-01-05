16532799a681717ec8493bcddb92adc82b192e63
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-05 20:05:33 +0200

    unit 3 "Directives", part 1: -creation the first directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 export interface Post {
     title: string;
@@ -11,26 +11,5 @@ export interface Post {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-    posts: Post[] = [
-        {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
-        {title: 'Следующий блок', text: 'Будет блок про директивы и еще про пайпы', id: 2}
-    ];
-
-    updatePost(post: Post) {
-        this.posts.unshift(post);
-        console.log('Post ', post);
-    }
-
-    removePost(id: number) {
-        console.log('Id to remove', id);
-        this.posts = this.posts.filter(p => p.id !== id);
-    }
-
-    ngOnInit(): void {
-        setTimeout(() => {
-            console.log('Timeout');
-            this.posts[0].title = 'Changed!';
-        }, 5000);
-    }
+export class AppComponent {
 }
