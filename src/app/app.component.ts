255765b6a8a067837f47d59e389fe5d3683057df
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-05 19:35:16 +0200

    unit components: part 3 components, livecycle hooks, Detection Strategy, styles with ViewEncapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Post {
     title: string;
@@ -11,7 +11,7 @@ export interface Post {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     posts: Post[] = [
         {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
         {title: 'Следующий блок', text: 'Будет блок про директивы и еще про пайпы', id: 2}
@@ -20,5 +20,17 @@ export class AppComponent {
     updatePost(post: Post) {
         this.posts.unshift(post);
         console.log('Post ', post);
+    }
+
+    removePost(id: number) {
+        console.log('Id to remove', id);
+        this.posts = this.posts.filter(p => p.id !== id);
+    }
+
+    ngOnInit(): void {
+        setTimeout(() => {
+            console.log('Timeout');
+            this.posts[0].title = 'Changed!';
+        }, 5000);
     }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,20 +1,44 @@
-import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ContentChild,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    SimpleChanges,
+    ViewEncapsulation
+} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
-    styleUrls: ['./post.component.scss']
+    styleUrls: ['./post.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit,
+    OnChanges {
     @Input() post: Post;
+    // tslint:disable-next-line:no-output-on-prefix
+    @Output() onRemove = new EventEmitter<number>();
     @ContentChild('info', {static: true}) infoRef: ElementRef;
-    constructor() {
-    }
 
     ngOnInit(): void {
-        console.log(this.infoRef.nativeElement);
+        console.log('ngOnInit');
+        //  console.log(this.infoRef.nativeElement);
     }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('ngOnChanges');
+    }
+
+
+    removePost() {
+        this.onRemove.emit(this.post.id);
+    }
 }
 
