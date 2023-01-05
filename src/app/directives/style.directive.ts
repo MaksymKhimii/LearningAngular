fb05fee426939b8823d209968240defd8009986c
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-05 20:21:39 +0200

    unit 4 "Directives", part 2: - adding events using @HostListener

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,23 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
+    constructor(private el: ElementRef, private r: Renderer2) {
+       // this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+        // el.nativeElement.style.color = 'red';
+    }
 
+    @HostListener('click', ['$event.target']) onClick(event: Event) {
+        console.log(event);
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null);
+    }
 }
