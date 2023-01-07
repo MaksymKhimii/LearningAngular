a97c7ed176dc7456a434c02f85ee4868953da56f
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-07 19:41:48 +0200

    unit 8 "Working with the server (HttpClient)", part 2: - RxJS operators; - removing and modifying elements; - moving logic  from component to a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {delay} from 'rxjs/operators';
+import {Todo, TodosService} from './todos.service';
 
-export interface Todo {
-    completed: boolean;
-    title: string;
-    id?: number;
-}
 
 @Component({
     selector: 'app-root',
@@ -14,15 +11,50 @@ export interface Todo {
 })
 export class AppComponent implements OnInit {
     todos: Todo[] = [];
+    loading = false;
 
-    constructor(private http: HttpClient) {
+    todoTitle = '';
+
+    constructor(private todosService: TodosService) {
     }
 
     ngOnInit() {
-        this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos/?_limit=2')
+        this.fetchTodos();
+    }
+
+    addTodo() {
+        if (!this.todoTitle.trim()) {
+            return;
+        }
+        this.todosService.addTodo({
+            title: this.todoTitle,
+            completed: false
+        }).subscribe(todo => {
+            this.todos.push(todo);
+            this.todoTitle = '';
+        });
+    }
+
+    fetchTodos() {
+        this.loading = true;
+        this.todosService.fetchTodos()
             .subscribe(todos => {
-                console.log('Response: ', todos);
                 this.todos = todos;
+                this.loading = false;
+            });
+    }
+
+    removeTodo(id: number) {
+        this.todosService.removeTodo(id)
+            .subscribe(response => {
+                this.todos = this.todos.filter(t => t.id !== id);
+            });
+    }
+
+    completeTodo(id: number) {
+        this.todosService.completeTodo(id)
+            .subscribe(todo => {
+                this.todos.find(t => t.id === todo.id).completed = true;
             });
     }
 }
