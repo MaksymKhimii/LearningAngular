d87f426df1a4cb53a10233fa6bc2d87b36825a9c
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-08 13:21:48 +0200

    unit 9 " Multi page with routing)", part 1, fixes: - fixed routing for PostsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
-import {PostComponent} from './post/post.component';
+import {PostsComponent} from './posts/posts.component';
 
 // http//localhost:4200/ -> HomeComponent
 // http//localhost:4200/about -> AboutComponent
@@ -11,7 +11,7 @@ import {PostComponent} from './post/post.component';
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'posts', component: PostComponent}
+    {path: 'posts', component: PostsComponent}
 ];
 
 @NgModule({
